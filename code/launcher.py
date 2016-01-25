6cd9e36d4dffc0e35f4f654f3351126e0b9e7343
Mikhail Dubov <mdubov@mirantis.com>
2016-01-26 00:32:51 +0100

    Add more documentation to launcher.py
    Add more comments to launcher.py to make it obvious how to use these scripts
    from Rhinopython.

--- code/launcher.py
+++ code/launcher.py
@@ -7,9 +7,21 @@ import rhinoscriptsyntax as rs
 
 
 if __name__ == '__main__':
-    # NOTE(mikhaildubov): The code to be executed in Rhinoceros should go here
+    # NOTE(mikhaildubov): The code to be executed in Rhinoceros should go here.
+    #
+    #                     To launch any of the commands below:
+    #                       1. Load the corresponding shape (curve/mesh) to Rhinoceros
+    #                       2. Type "_EditPythonScript" in the command prompt of Rhinoceros
+    #                       3. Load & run this python module
+    #                       4. Click on the shape loaded to the Rhinoceros editor
+    #                          to apply the corresponding transformation to it.
 
+    # MESHES
+
+    # Harmonic flow (Mean Curvature flow)
     #harmonic_flow.draw_motion_vectors(step=10)
     #flow_utils.iterate(harmonic_flow.flow, 200, step=0.05)
+
+    # Face flow
     #face_flow.draw_motion_vectors(step=10)
     flow_utils.iterate(face_flow.flow, 200, step=0.05)
